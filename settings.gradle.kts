d998bdb7ebb168001940488a0ba4b8bd9aa0ea0e
Ani Ivanyan <aniivanyan23@gmail.com>
2023-11-05 01:45:29 +0400

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mobile-HW2"
+include(":app")
+ 
