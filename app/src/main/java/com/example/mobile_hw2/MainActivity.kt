ac301d5023cc93649212522ddddd82f91328be03
Ani Ivanyan <aniivanyan23@gmail.com>
2023-11-05 18:55:31 +0400

    Final Done

--- app/src/main/java/com/example/mobile_hw2/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw2/MainActivity.kt
@@ -3,16 +3,8 @@ package com.example.mobile_hw2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.mobile_hw2.ui.theme.MobileHW2Theme
@@ -44,26 +36,11 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         if (!navController.navigateUp()) {
             super.onBackPressed()
         }
-    }
-}
-
-
-@Composable
-fun CityName(city: String) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = city,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
-        )
     }
 }
 
--- app/src/main/java/com/example/mobile_hw2/Utils.kt
+++ app/src/main/java/com/example/mobile_hw2/Utils.kt
@@ -1,7 +1,30 @@
 package com.example.mobile_hw2
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CityName(city: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = city,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
 fun getCityImage(city: String): Int {
     return when (city) {
